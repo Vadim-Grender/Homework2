8458534d251f6bc9572aa7d1ba86028bc6fa75a6
Vadim Sh <2423423@mail.ru>
2023-03-12 20:09:36 +0300

    create

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println(buildstring(8, 'a', 'b'));
+        System.out.println(buildstring(6, 'a', 'b'));
         CreateFile();
 
     }
@@ -18,10 +18,9 @@ public class Main {
     public static void CreateFile() throws IOException {
         BufferedOutputStream os = new BufferedOutputStream(new FileOutputStream("file.txt"));
         String text = "TEXT";
+        text = text.repeat(100);
         byte[] bytes = text.getBytes(StandardCharsets.UTF_8);
-        for (int i = 0; i < 100; i++) {
-            os.write(bytes);
-        }
+        os.write(bytes);
         os.flush();
     }
 }
